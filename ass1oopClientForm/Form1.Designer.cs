4fb64388efc24eae5e94bfc18b642396ea3b802f
W0471965 <joyceding007@gmail.com>
2023-09-22 11:59:20 -0300

    Add project files.

---
+++ ass1oopClientForm/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace ass1oopClientForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.labelConversation = new System.Windows.Forms.Label();
+            this.textBoxConversation = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 236);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Type your messages here";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.textBoxMessage.Location = new System.Drawing.Point(48, 255);
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(316, 20);
+            this.textBoxMessage.TabIndex = 2;
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(379, 252);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend.TabIndex = 3;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // labelConversation
+            // 
+            this.labelConversation.AutoSize = true;
+            this.labelConversation.Location = new System.Drawing.Point(48, 299);
+            this.labelConversation.Name = "labelConversation";
+            this.labelConversation.Size = new System.Drawing.Size(69, 13);
+            this.labelConversation.TabIndex = 4;
+            this.labelConversation.Text = "Conversation";
+            // 
+            // textBoxConversation
+            // 
+            this.textBoxConversation.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxConversation.Location = new System.Drawing.Point(48, 324);
+            this.textBoxConversation.Multiline = true;
+            this.textBoxConversation.Name = "textBoxConversation";
+            this.textBoxConversation.Size = new System.Drawing.Size(406, 242);
+            this.textBoxConversation.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 587);
+            this.Controls.Add(this.textBoxConversation);
+            this.Controls.Add(this.labelConversation);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBoxMessage);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBoxMessage;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.Label labelConversation;
+        private System.Windows.Forms.TextBox textBoxConversation;
+    }
+}
+
